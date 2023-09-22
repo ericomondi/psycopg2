e0513a04a897d760b824ffec2e25af7c7e0a2836
wycliffe <ericoochieng456@gmail.com>
2023-09-22 20:30:19 +0300

    update

--- dbservice.py
+++ dbservice.py
@@ -34,7 +34,7 @@ def get_data(table_name):
 sales = get_data("sales")
 prods = get_data("products")
 
-# # here we loop throgh the records 'list' to print each record
+# # here we loop throgh the prods 'list' to print each record
 for product in prods:
     print(product)
 
